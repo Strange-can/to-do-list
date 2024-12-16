d3e3ba77085d5722ecfb0c185688823d8f7ab0a0
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-12-16 22:19:42 +0800

    Change anchor tags to button tags
    The changes for the to-do list will be done by js through event listeners on buttons as it's not feasible to create new html files for every new project.

--- src/test.js
+++ src/test.js
@@ -1,3 +1,17 @@
-let greeting = 'hello'
+const projectPage = document.getElementById("project-page")
 
-export { greeting }
+const project1Button = document.getElementById("one")
+project1Button.addEventListener("click", () => {
+    projectPage.textContent = ''
+    oneText = document.createElement("p")
+    oneText.textContent = 'this is tasks for project 1'
+    projectPage.append(oneText)
+})
+
+const project2Button = document.getElementById("two")
+project2Button.addEventListener("click", () => {
+    projectPage.textContent = ''
+    twoText = document.createElement("p")
+    twoText.textContent = '222222222'
+    projectPage.append(twoText)
+})
